3ef03cec856585ba27ad9b87a7068ed0c4eda1ed
Bufuner <Bufuner@atguigu.com>
2022-09-08 09:06:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-09-08 8:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
